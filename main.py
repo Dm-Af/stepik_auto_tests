42211280a7b402b770e1d6501c871e11cfca428e
Dm-Af <mymailregsite@gmail.com>
2023-08-13 13:05:54 +0300

    Добавил файл lesson_3_2_8.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-print('Hello! It is main.py!')
-import lesson_2_4_7.py
-print('End of Main.py file!')
+# print('Hello! It is main.py!')
+# import lesson_2_4_7.py
+# print('End of Main.py file!')
+
+import sys
+from contextlib import contextmanager
+
+@contextmanager
+def reversed_print():
+    def reserved_write(text):
+        original_write(text[::-1])
+    original_write = sys.stdout.write
+    sys.stdout.write = reserved_write
+    yield
+    sys.stdout.write = original_write
+
+with reversed_print():
+    print('Вывод внутри блока with')
+
+print('Вывод вне блока with')
