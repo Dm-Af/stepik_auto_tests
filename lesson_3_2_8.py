6018b96d0254922adb82722d022aebb59d1b2825
Dm-Af <mymailregsite@gmail.com>
2023-08-13 13:08:16 +0300

    изменил файл lesson_3_2_8.py

--- lesson_3_2_8.py
+++ lesson_3_2_8.py
@@ -1,19 +1,4 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+def test_input_text(expected_result, actual_result):
+    assert expected_result==actual_result, f'expected {expected_result}, got {actual_result}'
 
-link = "http://suninjuly.github.io/simple_form_find_task.html"
-try:
-    #---------UBUNTU 22.04 - PyCharm - Don't work without this!---------
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--remote-debugging-port=9222")
-    browser = webdriver.Chrome(options=chrome_options)
-    #-------------------------------------------------------------------
-    browser.get(link)
-    button = browser.find_element(By.TAG_NAME, "button")
-    button.click()
-
-    time.sleep(10)
-
-finally:
-    browser.quit()
+test_input_text(input(), input())
