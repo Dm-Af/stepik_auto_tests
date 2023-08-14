b7d43f0760f273ed22c74c551ada7a0d96883668
Dm-Af <mymailregsite@gmail.com>
2023-08-14 13:07:38 +0300

    add file lesson_3_5_3__PyTest_test_marking_2marks.py, update file pytest.ini

---
+++ lesson_3_5_3__PyTest_test_marking_2marks.py
@@ -0,0 +1,45 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    # ---------UBUNTU 22.04 - PyCharm - Don't work without this!---------
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    browser = webdriver.Chrome(options=chrome_options)
+    # -------------------------------------------------------------------
+    yield browser
+    # этот код выполнится после завершения теста
+    print("\nquit browser..")
+    browser.quit()
+
+class TestMainPage1():
+
+    @pytest.mark.smoke
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    @pytest.mark.smoke
+    @pytest.mark.advanced_mark
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+# @pytest.mark.smoke и @pytest.mark.advanced_mark - декораторы, которые создают маркировку "mark" тестов,
+# чтобы их оба можно было вызывать из консоли, такой командой:
+# pytest -svm smoke lesson_3_5_2__PyTest_test_marking.py
+#
+# Чтобы вызвать только второй тест, нужно выполнить:
+# pytest -svm "smoke and advanced_mark" lesson_3_5_3__PyTest_test_marking_2marks.py
+#
+# за выбор теста по марке отвечает параметр -m
+# чтобы не выходило предупреждение о незарегистрированной марке, нужно их добавить в pytest.ini
+#
+# Так же можно маркировать целый тестовый класс.
+# В этом случае маркировка будет применена ко всем тестовым методам, входящим в класс.
