cb45fa1ea444a59ca78b947f11d97663f48843f6
Dm-Af <mymailregsite@gmail.com>
2023-08-14 19:31:14 +0300

    update file: test_3_6_3_test_parametrize.py

--- test_lesson_3_6/test_3_6_3_test_parametrize.py
+++ test_lesson_3_6/test_3_6_3_test_parametrize.py
@@ -6,3 +6,7 @@ def test_guest_should_see_login_link(browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/"
     browser.get(link)
     browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+# Можно задавать параметризацию также для всего тестового класса,
+# чтобы все тесты в классе запустились с заданными параметрами.
+# В таком случае отметка о параметризации должна быть перед объявлением класса
