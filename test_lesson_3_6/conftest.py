14f12e955dc59650673b6ab169242c2b3826b476
Dm-Af <mymailregsite@gmail.com>
2023-08-22 12:06:44 +0300

    modify conftest.py: add login_pass fixture

--- test_lesson_3_6/conftest.py
+++ test_lesson_3_6/conftest.py
@@ -10,19 +10,20 @@ import json
 
 @pytest.fixture(scope="function")
 def browser():
-    print("\nstart browser for test..")
+    # print("\nstart browser for test..")
     # ---------UBUNTU 22.04 - PyCharm - Don't work without this!---------
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--remote-debugging-port=9222")
     browser = webdriver.Chrome(options=chrome_options)
     # -------------------------------------------------------------------
-    browser.implicitly_wait(5) # ожидаю максимум 5 сек для каждого элемента
+    browser.implicitly_wait(10) # ожидаю максимум 5 сек для каждого элемента
     yield browser
-    print("\nquit browser..")
+    # print("\nquit browser..")
     browser.quit()
 
 @pytest.fixture(scope="function")
 def login_pass():
     with open('../my_login.json', 'r', encoding='utf-8') as file:
         my_login = json.load(file)
-    return my_login
+    yield my_login
+    # print("\n\nQuit fixture login_pass!")
