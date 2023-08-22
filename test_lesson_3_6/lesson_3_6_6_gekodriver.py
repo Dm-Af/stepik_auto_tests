93045ad8593d2fc9abf2253bb2fae2940490adb1
Dm-Af <mymailregsite@gmail.com>
2023-08-22 19:14:00 +0300

    update lesson_3_6_6_gekodriver.py

--- test_lesson_3_6/lesson_3_6_6_gekodriver.py
+++ test_lesson_3_6/lesson_3_6_6_gekodriver.py
@@ -7,3 +7,12 @@ browser = webdriver.Firefox(options=firefox_options)
 # browser = webdriver.Firefox()
 
 browser.get("https://stepik.org/lesson/25969/step/8")
+
+
+# из комментария на STEPIK-е:
+# from selenium.webdriver.firefox.options import Options
+#
+# options = Options()
+# options.binary_location = r'/usr/local/bin/firefox'
+# driver = webdriver.Firefox(executable_path=r'/usr/local/bin/geckodriver', options=options)
+# driver.get("https://stepik.org/lesson/25969/step/8")
